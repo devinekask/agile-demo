651ad17fb3823655e8e4d9f779a5b9750c3c23f0
Frederik Duchi <frederik.duchi@telenet.be>
2021-10-21 09:53:03 +0200

    asking an email for #1

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
 export const init = () => {
-    console.log('start executing JavaSript');
+  console.log('start executing JavaSript');
+  const email = 'frederik.duchi@howest.be';
+  console.log(email);
 };
